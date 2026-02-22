11fdf270f50e29fdce359737558e0f2cb55207d8
codeleaded <gnu.c.reaper@gmail.com>
2026-02-22 18:33:23 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -2,8 +2,15 @@
 #include "/home/codeleaded/System/Static/Library/CSV.h"
 
 int main(){
-    CSV rx = CSV_New("./data/Data.csv");
-    CSV_Print(&rx);
-    CSV_Free(&rx);
+    CSV csv = CSV_New();
+    CSV_DF(&csv,"./data/Data.csv");
+    
+    CSV_Print(&csv);
+    CSV_Save(&csv,"./data/Out.csv");
+
+    CSV_Table_Print(&csv);
+    CSV_Table_Save(&csv,"./data/Out_Table.txt");
+
+    CSV_Free(&csv);
     return 0;
 }
